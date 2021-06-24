8ff137639cb206b61421ded4b7f1cc4660342362
JonWaaler <jonwaaler@outlook.com>
2021-06-23 22:51:13 -0400

    post,put,delete added

--- controllers/routes/api/post-routes.js
+++ controllers/routes/api/post-routes.js
@@ -12,4 +12,80 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET 1 post
+router.get("/:id", (req, res) => {
+  Post.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((UserData) => res.json(UserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST new post
+router.post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    userId: req.body.userId,
+  })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "No post found with that ID." });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a tag by its `id` value
+router.delete("/:id", (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((fcData) => {
+      if (!fcData) {
+        res.status(404).json({ message: "No post found by that ID." });
+        return;
+      }
+      res.json(fcData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
