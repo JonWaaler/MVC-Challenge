bd82a26f926c89e0fda83c70d6e9293d54bdbe57
JonWaaler <jonwaaler@outlook.com>
2021-06-25 23:07:06 -0400

    dashboard post html/css, set-up for front-end posting to backend

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,13 @@
+/* POST HTML SET-UP
+<div class="row post mx-auto">
+    <h3 class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Debitis quaerat tempora ea cupiditate nisi, quo optio vero distinctio
+      consequuntur culpa!</h3>
+    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Dolorum nostrum laudantium ratione repellat voluptatem omnis
+      corporis, cumque officiis magnam aliquid, commodi qui quis! Illo animi
+      praesentium in aut maiores odio distinctio numquam sed corporis natus!
+      Saepe similique atque deleniti in modi, laboriosam optio ipsa non
+      voluptatum assumenda quos animi enim.</p>
+  </div>
+*/
